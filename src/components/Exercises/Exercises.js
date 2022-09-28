c40a99a569e66c1145c936a656eefd3c6012e8ff
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-09-28 21:30:47 +0600

    all components added

---
+++ src/components/Exercises/Exercises.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Exercises.css";
+
+const Exercises = () => {
+  return (
+    <div className="exercises">
+      <h1>ACE WORKOUT</h1>
+    </div>
+  );
+};
+
+export default Exercises;
