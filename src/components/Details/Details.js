7b0b9b7deb40a74add9a83fca8ce0f6cda37db8e
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-09-28 23:07:27 +0600

    details info and style added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,10 +1,30 @@
 import React from "react";
+import User from "../User/User";
 import "./Details.css";
 
 const Details = () => {
   return (
     <div className="details">
-      <h1>Details</h1>
+      <User></User>
+      <div className="details-functions">
+        <div className="add-break">
+          <h2>Add A Break</h2>
+          <div className="details-container">
+            <p>Button group</p>
+          </div>
+        </div>
+        <div className="exercise-details">
+          <h2>Exercise Details</h2>
+          <div className="details-container">
+            <p>Exercise time</p>
+            <p>200m</p>
+          </div>
+          <div className="details-container">
+            <p>Break time</p>
+            <p>15s</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/User/User.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./User.css";
+
+const User = () => {
+  return (
+    <div className="user-box">
+      {/* image and name */}
+      <div className="user">
+        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="user" />
+        <div className="user-detail">
+          <h3>Sunit Corneleous</h3>
+          <p>Dhaka, Bangladesh</p>
+        </div>
+      </div>
+      {/* user informations */}
+      <div className="user-info">
+        <div className="weight">
+          <h3>72 Kg</h3>
+          <p>Weight</p>
+        </div>
+        <div className="height">
+          <h3>6.5</h3>
+          <p>Height</p>
+        </div>
+        <div className="age">
+          <h3>23 years</h3>
+          <p>Age</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
