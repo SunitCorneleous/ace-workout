16d2f069103a0efb4bb41e396993bb52a2186046
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-09-28 23:51:48 +0600

    time state added

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -6,6 +6,7 @@ import "./Exercises.css";
 
 const Exercises = () => {
   const [exercises, setExercises] = useState([]);
+  const [time, setTime] = useState(0);
 
   useEffect(() => {
     fetch("data.json")
