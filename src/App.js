993c6773b0a284858dff7194a99dcc3b9c0883c7
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-09-28 21:43:50 +0600

    exercise state added and data fetched

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Details from "./components/Details/Details";
 import Exercises from "./components/Exercises/Exercises";
 
 function App() {
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then(res => res.json())
+      .then(data => setExercises(data));
+  }, []);
+
   return (
     <div className="App">
       <Exercises></Exercises>
