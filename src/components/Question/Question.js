475c12d613fdb0551d34b1c80056dce41cf8bfdb
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-09-29 14:07:40 +0600

    question component added

---
+++ src/components/Question/Question.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Question.css";
+
+const Question = ({ question }) => {
+  return (
+    <div>
+      <h2>{question.questions}</h2>
+      <p>{question.answer}</p>
+    </div>
+  );
+};
+
+export default Question;
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Question from "../Question/Question";
 import "./Questions.css";
 
 const Questions = () => {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("questions.json")
+      .then(res => res.json())
+      .then(data => setQuestions(data));
+  }, []);
+
   return (
     <div className="questions-container">
       <h1>Questions and anwsers</h1>
+      {questions.map(question => (
+        <Question key={question.id} question={question}></Question>
+      ))}
     </div>
   );
 };
